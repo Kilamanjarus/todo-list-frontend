7a9f4fded3ad3cbec1f12ecd45540d57ea67f013
max <max.lillie95@gmail.com>
2023-02-03 10:54:25 -0600

    customized css a small amount, added for loops on the todo list to have seperate titles/descript ands buttons, added delete button, and currently adding edit button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,10 +21,24 @@ export default {
     },
     postTodo: function () {
       this.newProject.status = "Unfinished"
-      this.newProject.date = Date.now()
+      this.newProject.date = Date()
       axios.post("http://localhost:3000/tasks", this.newProject).then(response => {
         console.log(response.data)
-        this.todoList = response.data
+        this.newProject = response.data
+        this.todoList.push(this.newProject)
+        this.newProject = {}
+      })
+    },
+    deleteTodo: function (todo) {
+      axios.delete(`http://localhost:3000/tasks/${todo.id}`).then(response => {
+        console.log(response.data)
+        this.todoList.splice(this.todoList.indexOf(todo), 1)
+      })
+    },
+    editTodo: function (todo) {
+      axios.delete(`http://localhost:3000/tasks/${todo.id}`).then(response => {
+        console.log(response.data)
+        this.todoList.splice(this.todoList.indexOf(todo), 1)
       })
     },
   },
@@ -34,16 +48,22 @@ export default {
 <template>
   <div class="add-todo">
     <h1>Welcome to your todo list! Please add some some projects!</h1>
-    <p>Title: </p>
+    <p><b>Title: </b></p>
     <input type="text" v-model="this.newProject.title" placeholder="Title..." />
-    <p>Description: </p>
+    <p><b>Description: </b></p>
     <textarea v-model="this.newProject.description" placeholder="Describe here..."> </textarea>
-    <p>Deadline: </p>
-    <input type="text" v-model="this.newProject.deadline" placeholder="Date..." />
+    <p><b>Deadline: </b></p>
+    <input type="text" v-model="this.newProject.deadline" placeholder="Year-month-date..." />
+    <p><button @click="postTodo(this.newProject)">Post Project</button></p>
   </div>
 
-  <div class="todo-list">
-    <h1 v-for="todo in this.todoList">{{ todo }}</h1>
+  <div class="todo-list" v-for="todo in this.todoList" v="if">
+    <h1>
+      <span> <button @click="editTodo(todo)">Edit</button></span>
+      {{ todo.title }}
+      <span> <button @click="deleteTodo(todo)"> Delete</button></span>
+    </h1>
+    <h2>{{ todo.description }}, due at {{ todo.deadline }}</h2>
   </div>
 </template>
 
